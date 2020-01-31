9e0e0fc75986e49b6b548e4cd8ab1cc855947889
micpob <bomboloapp@gmail.com>
2020-01-31 09:37:51 +0100

    Add notifications on opening bookmarks URLs

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
+let showBookmarkInfo = false
+let notificationsTimeout = undefined
 let startDate = 0
 let endDate = 0
 
 const bookmarkedUrlsArray = []
+const parentFoldersObject = {}
 
 let excludedFolders = []
 
@@ -15,18 +18,39 @@ function process_bookmark (bookmarks) {
     let bookmark = bookmarks[i]
     
     if (bookmark.url) {
-      if (bookmark.dateAdded >= startDate && bookmark.dateAdded <= endDate && !excludedFolders.includes(bookmark.parentId))
-      bookmarkedUrlsArray.push(bookmark.url)
+      if (bookmark.dateAdded >= startDate && bookmark.dateAdded <= endDate && !excludedFolders.includes(bookmark.parentId)) {
+        if (showBookmarkInfo) {
+          const dateAdded = new Date(bookmark.dateAdded).toLocaleDateString()
+          const folderId = bookmark.parentId
+          const parentFolderTitle = parentFoldersObject[folderId]
+          const urlObject = {
+            url: bookmark.url,
+            urlDate: dateAdded,
+            urlParentFolder: parentFolderTitle
+          } 
+          bookmarkedUrlsArray.push(urlObject) 
+        } else {
+          urlObject = {
+            url: bookmark.url
+          } 
+          bookmarkedUrlsArray.push(urlObject)
+        }        
+      }      
     }
 
     if (bookmark.children) {
+      if (showBookmarkInfo) {
+        const id = bookmark.id
+        const title = bookmark.title
+        parentFoldersObject[id] = title 
+        }
       process_bookmark(bookmark.children)
     }
   }
 
   recursionCounter--
 
-  if (recursionCounter === 0){
+  if (recursionCounter === 0) {
     openBookmark()
   }
 }
@@ -38,24 +62,51 @@ function openBookmark () {
     return
   }
   const randomIndex = Math.floor(Math.random() * bookmarkedUrlsArrayLength)
-  const randomUrl = bookmarkedUrlsArray[randomIndex]
-  window.open(randomUrl)  
+  const randomUrlObject = bookmarkedUrlsArray[randomIndex]
+  randomUrl = randomUrlObject.url
+  window.open(randomUrl)
+  if (showBookmarkInfo) {
+    chrome.notifications.clear('BookmarkRouletteUrlInfo')
+    chrome.notifications.create('BookmarkRouletteUrlInfo', {   
+      type: 'basic', 
+      iconUrl: 'Res/Icons/icon48.png', 
+      title: `Bookmarked on: ${randomUrlObject.urlDate} \nIn folder: ${randomUrlObject.urlParentFolder}`, 
+      message: ``,
+      priority: 1,
+      silent: true 
+      }, () => {
+        if (typeof notificationsTimeout != "undefined") {
+          clearTimeout(notificationsTimeout)        
+        }
+        notificationsTimeout = setTimeout(() => {
+          chrome.notifications.clear('BookmarkRouletteUrlInfo')
+        }, 4000) 
+      })
+  }
   bookmarkedUrlsArray.length = 0
 }
 
 chrome.browserAction.onClicked.addListener( () => {
-  chrome.storage.sync.get(['dateRangeObject'], (dates) => {
-    startDate = new Date(`${dates.dateRangeObject.startMonth} 01 ${dates.dateRangeObject.startYear}`)
-    startDate = startDate.getTime()
-    endDate = new Date(`${dates.dateRangeObject.endMonth} 01 ${dates.dateRangeObject.endYear}`)
-    endDate.setMonth(endDate.getMonth() + 1, 1)
-    endDate = endDate.getTime()
-    chrome.storage.sync.get(['excludedFolders'], (folderList) => {
-      if (folderList.excludedFolders) {
-        excludedFolders = folderList.excludedFolders
-      }
-      chrome.bookmarks.getTree( process_bookmark )
+  bookmarkedUrlsArray.length = 0
+  chrome.storage.sync.get('showInfo', (status) => {
+    if (status.showInfo && status.showInfo === 'on') {
+      console.log('status.showInfo: ', status.showInfo)      
+      showBookmarkInfo = true
+    } else {
+      showBookmarkInfo = false
+    }
+    chrome.storage.sync.get(['dateRangeObject'], (dates) => {
+      startDate = new Date(`${dates.dateRangeObject.startMonth} 01 ${dates.dateRangeObject.startYear}`)
+      startDate = startDate.getTime()
+      endDate = new Date(`${dates.dateRangeObject.endMonth} 01 ${dates.dateRangeObject.endYear}`)
+      endDate.setMonth(endDate.getMonth() + 1, 1)
+      endDate = endDate.getTime()
+      chrome.storage.sync.get(['excludedFolders'], (folderList) => {
+        if (folderList.excludedFolders) {
+          excludedFolders = folderList.excludedFolders
+        }
+        chrome.bookmarks.getTree( process_bookmark )
+      }) 
     }) 
   })  
-  
 })
