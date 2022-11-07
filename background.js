b4769c30666a7660026d1de5eb9f9a2ceb13521a
micpob <bomboloapp@gmail.com>
2022-11-07 11:33:20 +0100

    Switch to v3

--- background.js
+++ background.js
@@ -1,168 +1,4 @@
-let openInNewTab = false
-let showBookmarkInfo = true
-let notificationsTimeout = undefined
-let startDate = 0
-let endDate = 0
-
-const bookmarkedUrlsArray = []
-const parentFoldersObject = {}
-
-let excludedFolders = []
-
-let recursionCounter = 0
-
-const process_bookmark = (bookmarks) => {
-
-  recursionCounter++
-  
-  for (let i=0; i < bookmarks.length; i++) {    
-    let bookmark = bookmarks[i]
-    
-    if (bookmark.url) {
-      if (bookmark.dateAdded >= startDate && bookmark.dateAdded <= endDate && !excludedFolders.includes(bookmark.parentId)) {
-        if (showBookmarkInfo) {
-          const dateAdded = new Date(bookmark.dateAdded).toLocaleDateString('default', { year: 'numeric', month: 'long', day: 'numeric' })
-          const folderId = bookmark.parentId
-          const parentFolderTitle = parentFoldersObject[folderId]
-          const urlObject = {
-            url: bookmark.url,
-            urlDate: dateAdded,
-            urlParentFolder: parentFolderTitle
-          } 
-          bookmarkedUrlsArray.push(urlObject) 
-        } else {
-          urlObject = {
-            url: bookmark.url
-          } 
-          bookmarkedUrlsArray.push(urlObject)
-        }        
-      }      
-    }
-
-    if (bookmark.children) {
-      if (showBookmarkInfo) {
-        const id = bookmark.id
-        const title = bookmark.title
-        parentFoldersObject[id] = title 
-        }
-      process_bookmark(bookmark.children)
-    }
-  }
-
-  recursionCounter--
-
-  if (recursionCounter === 0) {
-    openBookmark()
-  }
-}
-
-const openBookmark = () => {
-  const bookmarkedUrlsArrayLength = bookmarkedUrlsArray.length
-  if (bookmarkedUrlsArrayLength < 1) {
-    const alertMessage = chrome.i18n.getMessage('no_bookmarks_alert') 
-    chrome.runtime.openOptionsPage(() => { alert(alertMessage) })    
-    return
-  }
-  const randomIndex = Math.floor(Math.random() * bookmarkedUrlsArrayLength)
-  const randomUrlObject = bookmarkedUrlsArray[randomIndex]
-  randomUrl = randomUrlObject.url
-  if (openInNewTab) {
-    chrome.tabs.create({ url: randomUrl })
-  } else {
-    chrome.tabs.update({ url: randomUrl })
-  }
-  if (showBookmarkInfo) {
-    const bookmarkedOn = chrome.i18n.getMessage('bookmarked_on_date') 
-    const inFolder = chrome.i18n.getMessage('saved_in_folder') 
-    chrome.notifications.clear('RandomBookmarkMachineInfo')
-    chrome.notifications.create('RandomBookmarkMachineInfo', {   
-      type: 'basic', 
-      iconUrl: 'Res/Icons/icon48.png', 
-      title: `${bookmarkedOn} ${randomUrlObject.urlDate} \n${inFolder} ${randomUrlObject.urlParentFolder}`, 
-      message: ``,
-      priority: 1,
-      silent: true 
-      }, () => {
-        if (typeof notificationsTimeout != "undefined") {
-          clearTimeout(notificationsTimeout)        
-        }
-        notificationsTimeout = setTimeout(() => {
-          chrome.notifications.clear('RandomBookmarkMachineInfo')
-        }, 4000) 
-      })
-  }
-  bookmarkedUrlsArray.length = 0
-}
-
-chrome.commands.onCommand.addListener( (command) => {
-  if (command === "get_random_bookmark") {
-    chrome.storage.sync.get('useShortcut', (status) => {
-      if (status.useShortcut && status.useShortcut === 'on') {        
-        getRandomBookmark()        
-      }
-    }) 
-  }     
-})
-
-chrome.action.onClicked.addListener( () => {
-  getRandomBookmark()
-})
-
-const getRandomBookmark = () => {
-  bookmarkedUrlsArray.length = 0
-  chrome.storage.sync.get('openInNewTab', (status) => {
-    if (status.openInNewTab && status.openInNewTab === 'on') {
-      openInNewTab = true
-    } else {
-      openInNewTab = false
-    }
-    chrome.storage.sync.get('showInfo', (status) => {
-      if (status.showInfo && status.showInfo === 'off') {
-        showBookmarkInfo = false
-      } else {
-        showBookmarkInfo = true
-      }
-      chrome.storage.sync.get(['dateRangeObject'], (dates) => {
-        if (dates.dateRangeObject) {
-          startDate = new Date(`${dates.dateRangeObject.startMonth + 1} 01 ${dates.dateRangeObject.startYear}`)
-          startDate = startDate.getTime()
-          let endMonth
-          let endYear
-          if (dates.dateRangeObject.endMonth.length < 1) {
-            const date = new Date()
-            endMonth = date.getMonth()
-            endYear = date.getFullYear()
-          } else {
-            const date = new Date()
-            endMonth = dates.dateRangeObject.endMonth
-            endYear = dates.dateRangeObject.endYear.length < 1 ? date.getFullYear() : dates.dateRangeObject.endYear 
-          }
-          endDate = new Date(`${endMonth + 1} 01 ${endYear}`)
-          endDate.setMonth(endDate.getMonth() + 1, 1)
-          endDate = endDate.getTime()
-        } else {
-          startDate = 0
-          endDate = new Date()
-          endDate.setMonth(endDate.getMonth() + 1, 1)
-          endDate = endDate.getTime()
-        }      
-        chrome.storage.sync.get(['excludedFolders'], (folderList) => {
-          if (folderList.excludedFolders) {
-            excludedFolders = folderList.excludedFolders
-          }
-          chrome.bookmarks.getTree( process_bookmark )
-        }) 
-      }) 
-    })
-  }) 
-}
-
-
-
-
 chrome.runtime.onInstalled.addListener((details) => {
-  /* const currentVersion = chrome.runtime.getManifest().version
-  const previousVersion = details.previousVersion */
   const reason = details.reason
 
   switch (reason) {
@@ -174,21 +10,27 @@ chrome.runtime.onInstalled.addListener((details) => {
           "showInfo": true,
           "useShortcut": false,
           "dateRangeObject": {"endMonth" : "", "endYear" : "", "startMonth" : 0, "startYear" : "2008"}
+        }, () => {
+          chrome.bookmarks.getTree( buildStorageBookmarksArray )
         })
         break;
      case 'update':
-        chrome.storage.sync.get(['excludedFolders', 'openInNewTab'], (result) => {
+        chrome.storage.sync.get(['excludedFolders', 'openInNewTab', 'allBookmarks', 'showInfo', 'useShortcut', 'dateRangeObject'], (result) => {
+          let allBookmarks = result.allBookmarks ? result.allBookmarks : []
           let excludedFolders = result.excludedFolders ? result.excludedFolders : []
           let openInNewTab = result.openInNewTab ? result.openInNewTab : false
           let showInfo = result.showInfo ? result.showInfo : true
           let useShortcut = result.useShortcut ? result.useShortcut : false
           let dateRangeObject = result.dateRangeObject ? result.dateRangeObject : {"endMonth" : "", "endYear" : "", "startMonth" : 0, "startYear" : "2008"}
           chrome.storage.local.set({
+            "allBookmarks": allBookmarks,
             "excludedFolders": excludedFolders,
             "openInNewTab": openInNewTab,
             "showInfo": showInfo,
             "useShortcut": useShortcut,
             "dateRangeObject": dateRangeObject
+          }, () => {
+            chrome.bookmarks.getTree( buildStorageBookmarksArray )
           })
         })
         break;
@@ -201,4 +43,155 @@ chrome.runtime.onInstalled.addListener((details) => {
         break;
   }
 
+})
+
+
+
+const buildStorageBookmarksArray = async (bookmarks) => {
+
+  const allBookmarksArray = []
+
+  //let recursionCounter = 0
+
+  const processBookmarks = (arrayOfBookmarks) => {
+    //recursionCounter++
+
+    for (let i=0; i < arrayOfBookmarks.length; i++) {   
+
+      let bookmark = arrayOfBookmarks[i]
+  
+      console.log('processing bookmarks...')
+      //console.log('recursion counter:', recursionCounter)
+      //console.log('allBookmarksArray:', allBookmarksArray)
+      
+      if (bookmark.url) {
+        console.log('bookmark:', bookmark)
+        //const dateAdded = new Date(bookmark.dateAdded).toLocaleDateString('default', { year: 'numeric', month: 'long', day: 'numeric' })
+        chrome.bookmarks.getSubTree( bookmark.parentId, result => {
+          const folderTitle = result[0].title
+          const bookmarkObject = {
+            url: bookmark.url,
+            urlDate: bookmark.dateAdded,
+            parentFolderTitle: folderTitle,
+            parentFolderId: bookmark.parentId
+          } 
+          allBookmarksArray.push(bookmarkObject)
+          chrome.storage.local.set({allBookmarks: allBookmarksArray}, () => {  });
+        })    
+      }
+  
+      if (bookmark.children) {
+        processBookmarks(bookmark.children)
+      }
+  
+    }
+  
+    //recursionCounter--
+  }
+
+  /* if (recursionCounter === 0) {
+    chrome.storage.local.get({allBookmarks: []}, (result) => {
+      var allBookmarks = allBookmarksArray;
+      chrome.storage.local.set({allBookmarks: allBookmarks}, () => {
+      });
+    });
+  } */
+
+  processBookmarks(bookmarks)
+}
+
+const openRandomBookmark = () => {
+  chrome.storage.local.get('allBookmarks', (result) => {
+    
+    const allBookmarks = result.allBookmarks
+    console.log(allBookmarks)
+
+    if (allBookmarks.length < 1) {
+      const alertMessage = chrome.i18n.getMessage('no_bookmarks_alert') 
+      chrome.runtime.openOptionsPage(() => { /* NOT WORKING ANYMORE IN v3 alert(alertMessage) */ })    
+      return
+    }
+  
+    chrome.storage.local.get(['openInNewTab', 'showInfo', 'dateRangeObject', 'excludedFolders'], (result) => {
+      const openInNewTab = result.openInNewTab
+      const showInfo = result.showInfo
+      const dateRangeObject = result.dateRangeObject
+      const excludedFolders = result.excludedFolders
+
+      //console.log(openInNewTab, showInfo, dateRangeObject, excludedFolders)
+
+      let startDate = new Date(`${dateRangeObject.startMonth + 1} 01 ${dateRangeObject.startYear}`)
+      startDate = startDate.getTime()
+      let endMonth
+      let endYear
+      if (dateRangeObject.endMonth.length < 1) {
+        const date = new Date()
+        endMonth = date.getMonth()
+        endYear = date.getFullYear()
+      } else {
+        const date = new Date()
+        endMonth = dateRangeObject.endMonth
+        endYear = dateRangeObject.endYear.length < 1 ? date.getFullYear() : dateRangeObject.endYear 
+      }
+      let endDate = new Date(`${endMonth + 1} 01 ${endYear}`)
+      endDate.setMonth(endDate.getMonth() + 1, 1)
+      endDate = endDate.getTime()
+
+      const filteredBookmarks = allBookmarks.filter( bookmark => !excludedFolders.includes(bookmark.parentFolderId) && bookmark.urlDate >= startDate && bookmark.urlDate <= endDate )
+
+      //console.log('filteredBookmarks:', filteredBookmarks)
+
+      const randomIndex = Math.floor(Math.random() * filteredBookmarks.length)
+      const randomUrlObject = filteredBookmarks[randomIndex]
+      const randomUrl = randomUrlObject.url
+      if (openInNewTab) {
+        chrome.tabs.create({ url: randomUrl })
+      } else {
+        chrome.tabs.update({ url: randomUrl })
+      }
+      if (showInfo) {
+        const dateAdded = new Date(randomUrlObject.urlDate).toLocaleDateString('default', { year: 'numeric', month: 'long', day: 'numeric' })
+        const bookmarkedOn = chrome.i18n.getMessage('bookmarked_on_date') 
+        const inFolder = chrome.i18n.getMessage('saved_in_folder') 
+        chrome.notifications.clear('RandomBookmarkMachineInfo')
+        chrome.notifications.create('RandomBookmarkMachineInfo', {   
+          type: 'basic', 
+          iconUrl: 'Res/Icons/icon48.png', 
+          title: `${bookmarkedOn} ${dateAdded} \n${inFolder} ${randomUrlObject.parentFolderTitle}`, 
+          message: ``,
+          priority: 1,
+          silent: true 
+          }, () => {
+            if (typeof notificationsTimeout != "undefined") {
+              clearTimeout(notificationsTimeout)        
+            }
+            notificationsTimeout = setTimeout(() => {
+              chrome.notifications.clear('RandomBookmarkMachineInfo')
+            }, 4000) 
+          })
+      }
+    
+    })
+  })
+}
+
+chrome.action.onClicked.addListener(() => { openRandomBookmark() })
+
+chrome.bookmarks.onChanged.addListener(() => { chrome.bookmarks.getTree( buildStorageBookmarksArray ) })
+chrome.bookmarks.onChildrenReordered.addListener(() => { chrome.bookmarks.getTree( buildStorageBookmarksArray ) })
+chrome.bookmarks.onCreated.addListener(() => { chrome.bookmarks.getTree( buildStorageBookmarksArray ) })
+chrome.bookmarks.onImportEnded.addListener(() => { chrome.bookmarks.getTree( buildStorageBookmarksArray ) })
+chrome.bookmarks.onMoved.addListener(() => { chrome.bookmarks.getTree( buildStorageBookmarksArray ) })
+chrome.bookmarks.onRemoved.addListener(() => { chrome.bookmarks.getTree( buildStorageBookmarksArray ) })
+
+//TODO: fix keyboard shortcut for v3
+chrome.commands.onCommand.addListener( (command) => {
+  console.log(command)
+  if (command === "get_random_bookmark") {
+    chrome.storage.sync.get('useShortcut', (result) => {
+      if (result.useShortcut) {        
+        openRandomBookmark()        
+      }
+    }) 
+  }     
 })
