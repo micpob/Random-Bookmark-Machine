69107d62d6fd3d788eaaaef6af7740eb5eb0b9fb
micpob <bomboloapp@gmail.com>
2024-07-14 01:43:01 +0200

    Fix language selection in options page

--- options.js
+++ options.js
@@ -1,6 +1,8 @@
 let language = chrome.i18n.getUILanguage()
 language = language.slice(0,2)
 
+language = ['es', 'it', 'fr', 'pt'].includes(language) ? language : 'en'
+
 const monthsLocalizations = {
   en: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
   es: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
