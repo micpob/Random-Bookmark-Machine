18d1d2c8be37b560b2d765fb35e78aa43484fe7a
micpob <bomboloapp@gmail.com>
2025-03-13 00:23:53 +0100

    Add link to other extensions

--- background.js
+++ background.js
@@ -268,3 +268,15 @@ const noResults = () => {
     })  
   })
 }
+
+chrome.contextMenus.create({
+  id: 'myOtherExtensions',
+  title: chrome.i18n.getMessage("context_menu_my_other_extensions"),
+  contexts: ['action']
+}, () => chrome.runtime.lastError)
+
+chrome.contextMenus.onClicked.addListener((clickData) => {
+  if (clickData.menuItemId == 'myOtherExtensions') {
+    chrome.tabs.create({ url: 'https://chromewebstore.google.com/search/micpob' })
+  }
+})
